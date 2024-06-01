b1133337f8ad23f98f68354db6c0c57cc4a36fe1
Dilek Karali <108486801+dilekbakar@users.noreply.github.com>
2024-06-01 14:44:50 +0300

    Create NumberToWordsHelper.cs
    NumberToWordsHelper will help us create dynamic structures in the view section.

---
+++ Helpers/NumberToWordsHelper.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace FurnitureProject.Helpers
+{
+    public static class NumberToWordsHelper
+    {
+        private static readonly string[] words =
+        {
+        "Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten",
+        "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen", "Twenty"
+    };
+
+        public static string NumberToWords(int number)
+        {
+            if (number < 0 || number >= words.Length)
+            {
+                throw new ArgumentOutOfRangeException("number");
+            }
+            return words[number];
+        }
+    }
+}
